13776635e07bfa6d0166d8d0a6b127a6bda78e2d
Cheryl Wilkins <42008873+Yay-SugaKookies@users.noreply.github.com>
2018-11-03 11:33:40 -0400

    Add files via upload

--- average/average/Form1.Designer.vb
+++ average/average/Form1.Designer.vb
@@ -22,9 +22,89 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtAvg = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.btnAvg = New System.Windows.Forms.Button()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.lblNum2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtAvg
+        '
+        Me.txtAvg.Location = New System.Drawing.Point(35, 213)
+        Me.txtAvg.Name = "txtAvg"
+        Me.txtAvg.Size = New System.Drawing.Size(182, 20)
+        Me.txtAvg.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(117, 103)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 1
+        Me.txtNum2.Text = "Input number..."
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(117, 43)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 2
+        Me.txtNum1.Text = "Input number..."
+        '
+        'btnAvg
+        '
+        Me.btnAvg.BackColor = System.Drawing.Color.FromArgb(CType(CType(252, Byte), Integer), CType(CType(249, Byte), Integer), CType(CType(154, Byte), Integer))
+        Me.btnAvg.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnAvg.Location = New System.Drawing.Point(86, 165)
+        Me.btnAvg.Name = "btnAvg"
+        Me.btnAvg.Size = New System.Drawing.Size(75, 23)
+        Me.btnAvg.TabIndex = 3
+        Me.btnAvg.Text = "average"
+        Me.btnAvg.UseVisualStyleBackColor = False
+        '
+        'lblNum1
+        '
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.Location = New System.Drawing.Point(32, 46)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(53, 13)
+        Me.lblNum1.TabIndex = 4
+        Me.lblNum1.Text = "Number 1"
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.Location = New System.Drawing.Point(32, 106)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(53, 13)
+        Me.lblNum2.TabIndex = 5
+        Me.lblNum2.Text = "Number 2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(199, Byte), Integer), CType(CType(252, Byte), Integer), CType(CType(154, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(251, 261)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.btnAvg)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtAvg)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents txtAvg As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents btnAvg As System.Windows.Forms.Button
+    Friend WithEvents lblNum1 As System.Windows.Forms.Label
+    Friend WithEvents lblNum2 As System.Windows.Forms.Label
 
 End Class
--- average/average/Form1.vb
+++ average/average/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
 
+    Private Sub btnAvg_Click(sender As Object, e As EventArgs) Handles btnAvg.Click
+        Dim num1 As Double = CDbl(txtNum1.Text)
+        Dim num2 As Double = CDbl(txtNum2.Text)
+        Dim avg As Double = (num1 + num2) / 2
+        txtAvg.Text = ("The average of " & num1 & " and " & num2 & " is " & avg & ".")
+    End Sub
 End Class
