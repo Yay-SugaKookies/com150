fb1fd087cae072e997d5fca482dee7d9beaa6e40
Cheryl Wilkins <42008873+Yay-SugaKookies@users.noreply.github.com>
2018-11-03 11:14:31 -0400

    Add files via upload

---
+++ circle/WindowsApplication1/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub btnFindArea_Click(sender As Object, e As EventArgs) Handles btnFindArea.Click
+        Dim radius As Double = CDbl(txtRadius.Text)
+        Const pi As Double = 3.14
+        Dim area As Double = pi * radius ^ 2
+        txtArea.Text = ("The area of a circle is " & area & ".")
+    End Sub
+End Class
