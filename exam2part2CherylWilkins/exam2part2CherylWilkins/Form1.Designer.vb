1c61bccd45d0e1ad41b90486d6e333e5aa48fec6
Cheryl Wilkins <42008873+Yay-SugaKookies@users.noreply.github.com>
2019-01-04 15:38:23 -0500

    Add files via upload

---
+++ exam2part2CherylWilkins/exam2part2CherylWilkins/Form1.Designer.vb
@@ -0,0 +1,130 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.rb1 = New System.Windows.Forms.RadioButton()
+        Me.rb2 = New System.Windows.Forms.RadioButton()
+        Me.rb3 = New System.Windows.Forms.RadioButton()
+        Me.btnVote = New System.Windows.Forms.Button()
+        Me.btnResults = New System.Windows.Forms.Button()
+        Me.lstresult = New System.Windows.Forms.ListBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'rb1
+        '
+        Me.rb1.AutoSize = True
+        Me.rb1.Location = New System.Drawing.Point(17, 19)
+        Me.rb1.Name = "rb1"
+        Me.rb1.Size = New System.Drawing.Size(46, 17)
+        Me.rb1.TabIndex = 0
+        Me.rb1.TabStop = True
+        Me.rb1.Text = "Blue"
+        Me.rb1.UseVisualStyleBackColor = True
+        '
+        'rb2
+        '
+        Me.rb2.AutoSize = True
+        Me.rb2.Location = New System.Drawing.Point(17, 42)
+        Me.rb2.Name = "rb2"
+        Me.rb2.Size = New System.Drawing.Size(45, 17)
+        Me.rb2.TabIndex = 1
+        Me.rb2.TabStop = True
+        Me.rb2.Text = "Red"
+        Me.rb2.UseVisualStyleBackColor = True
+        '
+        'rb3
+        '
+        Me.rb3.AutoSize = True
+        Me.rb3.Location = New System.Drawing.Point(17, 64)
+        Me.rb3.Name = "rb3"
+        Me.rb3.Size = New System.Drawing.Size(55, 17)
+        Me.rb3.TabIndex = 2
+        Me.rb3.TabStop = True
+        Me.rb3.Text = "Purple"
+        Me.rb3.UseVisualStyleBackColor = True
+        '
+        'btnVote
+        '
+        Me.btnVote.Location = New System.Drawing.Point(23, 120)
+        Me.btnVote.Name = "btnVote"
+        Me.btnVote.Size = New System.Drawing.Size(75, 23)
+        Me.btnVote.TabIndex = 6
+        Me.btnVote.Text = "Vote"
+        Me.btnVote.UseVisualStyleBackColor = True
+        '
+        'btnResults
+        '
+        Me.btnResults.Location = New System.Drawing.Point(140, 19)
+        Me.btnResults.Name = "btnResults"
+        Me.btnResults.Size = New System.Drawing.Size(75, 23)
+        Me.btnResults.TabIndex = 7
+        Me.btnResults.Text = "Results"
+        Me.btnResults.UseVisualStyleBackColor = True
+        '
+        'lstresult
+        '
+        Me.lstresult.FormattingEnabled = True
+        Me.lstresult.Location = New System.Drawing.Point(119, 48)
+        Me.lstresult.Name = "lstresult"
+        Me.lstresult.Size = New System.Drawing.Size(120, 95)
+        Me.lstresult.TabIndex = 8
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rb1)
+        Me.GroupBox1.Controls.Add(Me.rb2)
+        Me.GroupBox1.Controls.Add(Me.rb3)
+        Me.GroupBox1.Location = New System.Drawing.Point(-3, 28)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(116, 86)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Vote:"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(251, 159)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.lstresult)
+        Me.Controls.Add(Me.btnResults)
+        Me.Controls.Add(Me.btnVote)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents rb1 As System.Windows.Forms.RadioButton
+    Friend WithEvents rb2 As System.Windows.Forms.RadioButton
+    Friend WithEvents rb3 As System.Windows.Forms.RadioButton
+    Friend WithEvents btnVote As System.Windows.Forms.Button
+    Friend WithEvents btnResults As System.Windows.Forms.Button
+    Friend WithEvents lstresult As System.Windows.Forms.ListBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ exam2part2CherylWilkins/exam2part2CherylWilkins/Form1.vb
@@ -0,0 +1,29 @@
+﻿Public Class Form1
+    Dim color() As String = {"blue", "red", "purple"}
+    Dim vote() As Integer = {0, 0, 0}
+    Dim win As Integer = 0
+    Private Sub btnVote_Click(sender As Object, e As EventArgs) Handles btnVote.Click
+        If rb1.Checked = True Then
+            vote(0) += 1
+        End If
+        If rb2.Checked = True Then
+            vote(1) += 1
+        End If
+        If rb3.Checked = True Then
+            vote(2) += 1
+        End If
+        For i = 0 To 2 Step 1
+            If win < vote(i) Then
+                win = i
+            End If
+        Next
+    End Sub
+
+    Private Sub btnResults_Click(sender As Object, e As EventArgs) Handles btnResults.Click
+        lstresult.Items.Clear()
+        lstresult.Items.Add("The superior color is " & color(win) & "!")
+        For x = 0 To 2 Step 1
+            lstresult.Items.Add(color(x) & ": " & vote(x))
+        Next
+    End Sub
+End Class
